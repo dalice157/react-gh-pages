8156d0f65e438648eb72d88849a240fcadcaac6c
Da Chu <dalice157@gmail.com>
2021-08-04 15:41:21 +0800

    修改文案

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          DA 的佈置練習
         </p>
         <a
           className="App-link"
